8bec4e475726558132b37745330488e08f4f7ecc
Lance Conry <lance@rhinosw.com>
2023-08-23 12:13:26 +1000

    Cleaned up download messages and sorting.

--- downloadHourlyUsage.js
+++ downloadHourlyUsage.js
@@ -3,35 +3,35 @@ import RxJs from "rxjs";
 import RxFetch from "rxjs/fetch";
 import RxJsOps from "rxjs/operators";
 
-const globalConfig = JSON.parse(fs.readFileSync(config.dataFile));
-
 const config = {
-	authToken: globalConfig.auroraAccount.authToken,
-	serivceAgreementId: globalConfig.serivceAgreementId,
-	customerId: globalConfig.customerId,
 	baseUrl: "https://api.auroraenergy.com.au/api/usage/day",
-	numDaysToRetrieve: 10,
 	parallellRequests: 5,
 	outputFile: 'output.json'
 };
 
-const baseUrl = `${config.baseUrl}?serviceAgreementID=${config.serivceAgreementId}&customerId=${config.customerId}&index=-`;
+const globalConfig = JSON.parse(fs.readFileSync('./config.json'));
+
+const baseUrl = `${config.baseUrl}?serviceAgreementID=${globalConfig.download.serivceAgreementId}&customerId=${globalConfig.download.customerId}&index=-`;
 
 const dayIndexList = [];
-for( let cI=1; cI < config.numDaysToRetrieve+1; dayIndexList.push(cI++) );
+for( let cI=1; cI < globalConfig.download.numDaysToRetrieve+1; dayIndexList.push(cI++) );
 
 const dayList = [];
 
 const get = dayIndex => {
 	const url = baseUrl + dayIndex;
 
-	console.log(`${dayIndex} ====>`);
 	return RxFetch.fromFetch(url, {
-		headers: {Authorization: `Bearer ${config.authToken}`}
+		headers: {Authorization: `Bearer ${globalConfig.download.authToken}`}
 	}).pipe(
-		RxJsOps.tap( response => console.log(`  ====> ${dayIndex}`	)),
 		RxJsOps.mergeMap( response => response.json() ),
-		RxJsOps.map( data => dayList.push(data))
+		RxJsOps.retry(2),
+		RxJsOps.tap( data => console.log(` ==> ${dayList.length+1}`)),
+		RxJsOps.map( data => dayList.push(data)),
+		RxJsOps.catchError( err => {
+			console.error(`${dayIndex}: ${JSON.stringify(err)}`);
+			return RxJs.of(err);
+		})
 	);
 
 };
@@ -39,11 +39,16 @@ const get = dayIndex => {
 RxJs.from(dayIndexList).pipe(
 	RxJsOps.mergeMap( (dayIndex, parallellRequests) => get(dayIndex), config.parallellRequests),
 ).subscribe( {
-	complete: () => {		
-		fs.writeFileSync(config.outputFile, JSON.stringify(dayList, null, 2));
+	error: err => console.error(err),
+	complete: () => {	
+		
+		const filteredList = dayList.filter(v=> v.StartDate !== undefined);
+		filteredList.sort( (a, b) => b.StartDate.localeCompare(a.StartDate));
+
+		fs.writeFileSync(config.outputFile, JSON.stringify(filteredList, null, 2));
+
+		console.log( `Complete.  Output saved to ${config.outputFile}.`)
+
 	}
 });
 
-dayList.sort( (a, b) => a.StartDate - b.StartDate);
-
-console.log( `Complete.  Output saved to ${config.outputFile}.`)
